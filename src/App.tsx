69db273907541f166b2fb6a238d7e2a76701cca8
mohammadhossein-6623 <mohammadhossein.rezai.66@gmail.com>
2025-05-12 15:07:09 +0430

    recoding

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import { Routes, Route, HashRouter } from "react-router-dom";
 import Products from "./page/productsPage/Products";
 import ProductDetails from "./component/productDetails/ProductDetails";
-// import { CartProvider } from "./context/CartContext";
 import LandingPage from "./page/landing/LandingPage";
 
 function App() {
   return (
     <HashRouter>
-      {/* <CartProvider> */}
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/products" element={<Products />} />
         <Route path="/products/:id" element={<ProductDetails />} />
       </Routes>
-      {/* </CartProvider> */}
     </HashRouter>
   );
 }
